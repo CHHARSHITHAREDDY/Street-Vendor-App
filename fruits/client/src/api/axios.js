5d82d7e50a62f089b838c0ef104feb225fd360de
harshitha <harshitha.chandireddy@gmail.com>
2025-11-14 15:33:08 +0530

    final changess

--- fruits/client/src/api/axios.js
+++ fruits/client/src/api/axios.js
@@ -13,19 +13,4 @@ api.interceptors.request.use(config => {
   return config;
 });
 
-// export default api;
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,   // ✅ Vite correct syntax
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default api;
+export default api;
